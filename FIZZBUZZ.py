bf649c84f8070ce7f543492dbc2b51603aac607b
CHAKRIKA-SAI-D <120378173+CHAKRIKA-SAI-D@users.noreply.github.com>
2023-04-19 19:10:17 +0530

    fizzbuzz.py

---
+++ FIZZBUZZ.py
@@ -0,0 +1,11 @@
+num=int(input("enter the number : "))
+if(num%3==0 and num%5==0):
+    print("FIZZBUZZ")
+elif (num%3==0):
+    print("FIZZ")
+elif(num%5==0):
+    print("BUZZ")
+    
+  output:
+    enter the number : 15
+    FIZZBUZZ
