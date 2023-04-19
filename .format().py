e593dc5ff2bceafce9306c232dd2eb7c39e06935
CHAKRIKA-SAI-D <120378173+CHAKRIKA-SAI-D@users.noreply.github.com>
2023-04-19 20:18:38 +0530

    .format().py

---
+++ .format().py
@@ -0,0 +1,7 @@
+a,b,c,d=[int(x) for x in  input("enter 4 numbers : ").split()]
+print("a is {} b is {} c is {} d is {}".format(a,b,c,d))
+
+
+output:
+enter 4 numbers : 1 2 3 4
+a is 1 b is 2 c is 3 d is 4
