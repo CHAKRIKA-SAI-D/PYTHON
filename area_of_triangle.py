331865fa592788e40b9938711c13ad3fdf61a231
CHAKRIKA-SAI-D <120378173+CHAKRIKA-SAI-D@users.noreply.github.com>
2023-04-19 20:48:17 +0530

    area of triangle

---
+++ area_of_triangle.py
@@ -0,0 +1,10 @@
+base=int(input("enter the base: "))
+height=int(input("enter the height : "))
+area=(base*height)/2
+print("area is ",area)
+
+
+output :
+enter the base : 5
+enter the height:3
+area is 7.5
