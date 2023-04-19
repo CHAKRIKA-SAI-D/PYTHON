3b5eae32f83098f3420fc013c7226445b54129b3
CHAKRIKA-SAI-D <120378173+CHAKRIKA-SAI-D@users.noreply.github.com>
2023-04-19 18:57:00 +0530

    range.py

---
+++ range.py
@@ -0,0 +1,15 @@
+num=int(input("enter the number : "))
+l_lim=int(input("enter the lower limit: "))
+u_lim=int(input("enter the upper limit : "))
+if num in range(l_lim,u_lim):
+    print("in range!!")
+else:
+    print("out of range!!")
+    
+    output:
+      enter the number :3
+      enter the lower limit: 0
+      enter the upper limit : 5
+      in range!!
+      
+      
