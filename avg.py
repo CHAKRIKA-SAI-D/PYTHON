84d03fd9478bcdb17ea023f81a1ba7606ce6a39b
CHAKRIKA-SAI-D <120378173+CHAKRIKA-SAI-D@users.noreply.github.com>
2023-04-19 20:28:01 +0530

    avg.py

---
+++ avg.py
@@ -0,0 +1,17 @@
+print("enter 5 subject marks : ")
+tot=0
+for i in range(0,5):
+    x=int(input())
+    tot=tot+x
+print("total is {} and average is {}".format(tot,tot/5))
+
+
+output :
+enter 5 subject marks :
+25
+24
+23
+22
+21
+total is 115 and average is 23.0
+    
