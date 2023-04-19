b0209191d468c4f71416704d0d1a6a65873cd77b
CHAKRIKA-SAI-D <120378173+CHAKRIKA-SAI-D@users.noreply.github.com>
2023-04-19 18:28:56 +0530

    input_type.py

---
+++ input_type.py
@@ -0,0 +1,12 @@
+a=input("enter the character: ")
+if (a>="a" and a<="z"):
+    print("lower case")
+elif (a>="A" and a<="Z"):
+    print("upper case")
+else:
+    print("digit")
+    
+    
+output:
+  enter the character: 6
+    digit
