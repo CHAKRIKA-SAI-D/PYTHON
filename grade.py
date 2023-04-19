825046d3c835e9080a2ed74725bc47df772e52f7
CHAKRIKA-SAI-D <120378173+CHAKRIKA-SAI-D@users.noreply.github.com>
2023-04-19 19:46:07 +0530

    grade.py

---
+++ grade.py
@@ -0,0 +1,16 @@
+marks=int(input("enter the marks: "))
+if marks>75:
+    print("O-grade")
+elif marks>60 and marks<75:
+    print("A-grade")
+elif marks>50 and marks<60:
+    print("B-grade")
+elif marks>35 and marks<50:
+    print("C-grade")
+else:
+    print("F-grade")
+    
+    
+ output:
+  enter the marks:99
+  O-grade
