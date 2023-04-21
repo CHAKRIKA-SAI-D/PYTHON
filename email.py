8341ffe5bc98c4d196758d59bca32a14f368c8b7
CHAKRIKA-SAI-D <120378173+CHAKRIKA-SAI-D@users.noreply.github.com>
2023-04-21 18:08:22 +0530

    information from user

---
+++ email.py
@@ -0,0 +1,13 @@
+name=input("enter the name : ")
+add=input("enter the address : ")
+email=input("enter the email :")
+phoneno=int(input("enter the phone number : "))
+print("name is {} address is {} email is {} phone number is : {}".format(name,add,email,phoneno))
+
+
+#output:
+enter the name : om
+enter the address : india
+enter the email :123@gmail.com
+enter the phone number : 123456789
+name is om address is india email is 123@gmail.com phone number is : 123456789
