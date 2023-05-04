689e4c403a9ee3c1ad8b3f86733d34ff44958307
CHAKRIKA-SAI-D <120378173+CHAKRIKA-SAI-D@users.noreply.github.com>
2023-05-04 19:57:38 +0530

    Gcd recursion

--- GCD.py
+++ GCD.py
@@ -1,15 +1,15 @@
-def hcfnaive(a, b):
+def gcd(a, b):
 	if(b == 0):
 		return abs(a)
 	else:
-		return hcfnaive(b, a % b)
+		return gcd(b, a % b)
 
 a = int(input("enter first number : "))
-b = int(input("enter the seconf number: "))
+b = int(input("enter the second number: "))
 print("The gcd of {} and {} is : ".format(a,b), end="")
-print(hcfnaive(a,b))
+print(gcd(a,b))
 
 #output:
 enter first number : 60
-enter the seconf number: 48
+enter the second number: 48
 The gcd of 60 and 48 is : 12
